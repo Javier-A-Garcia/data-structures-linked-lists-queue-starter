013aa4c2f104aa9359997fb2d7a4ea342231b6e7
Javier Garcia <javiergarcia7991@gmail.com>
2022-12-12 20:23:12 -0800

    Passing test 2

--- implementation/02-doubly-linked-list.js
+++ implementation/02-doubly-linked-list.js
@@ -21,8 +21,8 @@ class DoublyLinkedList {
         // Add node of val to head of linked list
         let newNode = new DoublyLinkedNode(val);
 
-        if (this.length >= 0) {
-            this.head.previous = newNode;
+        if (this.length > 0) {
+            this.head.prev = newNode;
             newNode.next = this.head;
             this.head = newNode;
         } else {
@@ -35,30 +35,59 @@ class DoublyLinkedList {
 
     addToTail(val) {
         // Add node of val to tail of linked list
+        let newNode = new DoublyLinkedNode(val);
 
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            newNode.prev = this.tail;
+            this.tail.next = newNode;
+        }
+
+        this.tail = newNode;
+        this.length++;
         // Write your hypothesis on the time complexity of this method here
     }
 
     removeFromHead() {
         // Remove node at head
+        if (!this.head) return;
 
+        let removed = this.head;
+        this.head = this.head.next;
+        if (this.head) { this.head.prev = null; }
+        this.length--;
+        return removed.value;
         // Write your hypothesis on the time complexity of this method here
     }
 
     removeFromTail() {
         // Remove node at tail
+        if(!this.tail) return;
+        
+        let removed = this.tail;
+
+        this.tail = this.tail.prev;
+        if (this.tail) {
+            this.tail.next = null;
+        }
+        
+        this.length--;
+        return removed.value;
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtHead() {
         // Return value of head node
-
+        if (this.head) return this.head.value;
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtTail() {
         // Return value of tail node
+
+        if (this.tail) return this.tail.value;
         
         // Write your hypothesis on the time complexity of this method here
     }
