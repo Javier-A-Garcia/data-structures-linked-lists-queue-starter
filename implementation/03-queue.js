df0d0092830e69bee8a5a6f4b242abff2d2be46f
Javier Garcia <javiergarcia7991@gmail.com>
2022-12-12 21:07:28 -0800

    Finished all "implementation" tests

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,17 +5,42 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+         }
+
+         this.length++;
+         return this.length;
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
-        
+        if (!this.tail) return null;
+
+        let removed = this.head;
+
+        if (this.length > 1) {
+            this.head = this.head.next;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+
+        this.length--;
+        return removed.value;
         // Write your hypothesis on the time complexity of this method here
     }
 
